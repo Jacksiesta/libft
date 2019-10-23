0d0f8ea678b104566fb5b728cbf1aaa55b803a91
Jacksiesta <56738437+Jacksiesta@users.noreply.github.com>
2019-10-23 20:37:06 +0200

    Add files via upload

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,46 +6,41 @@
 /*   By: jherrald <jherrald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 13:43:54 by jherrald          #+#    #+#             */
-/*   Updated: 2019/10/23 17:43:42 by jherrald         ###   ########.fr       */
+/*   Updated: 2019/10/23 19:58:20 by jherrald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_check(char c, const *set)
+static int	ft_check(char const c, char const *set)
 {
-	int x;
-
-	x = 0;
-	while (set[x])
+	while (*set)
 	{
-		if (c == set[x])
+		if (c == *set)
 			return (1);
-		x++;
+		set++;
 	}
 	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	// char	*new;
-	// size_t	len;
-	// size_t 	x;
+	char	*new;
+	size_t	len;
+	size_t 	x;
 
-	// if (!s1 || !set)
-	// 	return (NULL);
-	// len = ft_strlen(s1);
-	// x = 0;
-	// while (ft_check(s1[x], set))
-	// 	x++;
-	// while (ft_check(s1[len], set))
-	// 	len--;
-	// if (!s1)
-	// 	return (0);
-	// if (!(new = (char *)malloc(sizeof(char) * (len + 1)))
-	// 	return (NULL);
-	// new = ft_substr(s1, x, len - x);
-	// if (new == 0)
-	// 	return ("\0");
-	// return (new);
+	if (!s1 || !set)
+		return (0);
+	len = ft_strlen(s1);
+	x = 0;
+	while (ft_check(s1[x], set))
+		x++;
+	while (ft_check(s1[len], set))
+		len--;
+	len = len - x;
+	new = ft_substr(s1, x, len);
+	if (new == 0)
+		return ("\0");
+	new[len] = '\0';
+	return (new);
 }
